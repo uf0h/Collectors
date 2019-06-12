551dca36340017f94f69e630ffdb65b897b6aafe
uf0h <3p6824cbrf@gmail.com>
2019-06-12 20:47:47 +0100

    cleanup collector commands

--- src/main/java/me/ufo/collectors/commands/CollectorsCommand.java
+++ src/main/java/me/ufo/collectors/commands/CollectorsCommand.java
@@ -7,6 +7,7 @@ import me.ufo.collectors.collector.CollectionType;
 import me.ufo.collectors.collector.Collector;
 import me.ufo.collectors.item.CollectorItem;
 import me.ufo.collectors.util.Style;
+import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -32,77 +33,69 @@ public class CollectorsCommand implements CommandExecutor {
           "&e/collector remove &7- &dRemoves a collector from the chunk you are standing in.");
     }
 
-    switch (args.length) {
-      case 1:
-        if (args[0].equalsIgnoreCase("remove") && sender instanceof Player) {
-          final Player player = (Player) sender;
-          if (Collector.chunkHasCollector(player.getLocation())) {
-            final Location location = Collector.get(player.getLocation()).getLocation();
-            Collector.get(player.getLocation()).remove(true);
-            player.sendMessage(ChatColor.RED.toString() + "Collector removed from x: " + location.getBlockX() + ", y: " + location.getBlockY() + ", z: " + location.getBlockZ() + ".");
-          } else {
-            player.sendMessage(ChatColor.RED.toString() + "There is no collector in this chunk.");
+    switch (args[0].toLowerCase()) {
+      case "remove": {
+        if (sender instanceof Player) {
+          final Location location = ((Player) sender).getLocation();
+          if (Collector.chunkHasCollector(location)) {
+            Collector.get(location).remove(true);
+            return Style.message(sender,
+                ChatColor.RED.toString() + "Collector removed from x: " + location.getBlockX() + ", y: " + location.getBlockY() + ", z: " + location.getBlockZ() + ".");
           }
-          return false;
+          return Style.message(sender, ChatColor.RED.toString() + "There is no collector in this chunk.");
         }
         break;
-      case 2:
-      case 3:
-        if (args[0].equalsIgnoreCase("give")) {
-          final Player target = plugin.getServer().getPlayer(args[1]);
-          if (target == null) {
-            sender.sendMessage(ChatColor.RED.toString() + "That player cannot be found.");
-            return false;
-          }
+      }
 
-          int amount;
-          try {
-            amount = Integer.parseInt(args[2]);
-          } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-            amount = 1;
-          }
-
-          target.getInventory().addItem(new ItemStack(CollectorItem.get(amount)));
-
-          target.sendMessage(Style.translate("&dYou have been given " + amount + " collector(s)."));
+      case "give": {
+        final Player target = plugin.getServer().getPlayer(args[1]);
+        if (target == null) {
+          sender.sendMessage(ChatColor.RED.toString() + "That player cannot be found.");
           return false;
         }
 
-        if (args[0].equalsIgnoreCase("set") && sender instanceof Player) {
-          final Player player = (Player) sender;
+        int amount;
+        try {
+          amount = NumberUtils.createInteger(args[2]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+          amount = 1;
+        }
 
-          final CollectionType collectionType;
-          try {
-            collectionType = CollectionType.valueOf(args[1]);
-          } catch (Exception e) {
-            player.sendMessage(ChatColor.RED.toString() + "That is not a valid collection type.");
-            player.sendMessage(ChatColor.YELLOW.toString() + Arrays.toString(CollectionType.values()));
-            return false;
-          }
+        target.getInventory().addItem(new ItemStack(CollectorItem.get(amount)));
+        return Style.message(target, "&dYou have been given " + amount + " collector(s).");
+      }
 
-          final int amount;
-          try {
-            amount = Integer.parseInt(args[2]);
-          } catch (Exception e) {
-            player.sendMessage(ChatColor.RED.toString() + "That is not a amount.");
-            return false;
-          }
+      case "set": {
+        final CollectionType collectionType;
+        try {
+          collectionType = CollectionType.valueOf(args[1]);
+        } catch (Exception e) {
+          return Style.message(sender,
+              ChatColor.RED.toString() + "That is not a valid collection type.",
+              ChatColor.YELLOW.toString() + Arrays.toString(CollectionType.values()));
+        }
 
-          if (Collector.chunkHasCollector(player.getLocation())) {
-            if (Collector.isCollector(player.getLocation().subtract(new Vector(0, 0.5, 0)).getBlock().getLocation())) {
-              final Collector collector = Collector.get(player.getLocation().subtract(new Vector(0, 0.5, 0)).getBlock().getLocation());
-              collector.getAmounts().entrySet().stream().filter(entry -> collectionType == entry.getKey()).forEach(entry -> entry.setValue(amount));
-              player.sendMessage(ChatColor.RED.toString() + "Collection type " + ChatColor.YELLOW.toString() + collectionType.toString() + ChatColor.RED.toString() + " has been set to " + ChatColor.YELLOW.toString() + amount + ChatColor.RED.toString() + ".");
-            } else {
-              player.sendMessage(ChatColor.RED.toString() + "This is not this chunks collector.");
-            }
+        final int amount;
+        try {
+          amount = NumberUtils.createInteger(args[2]);
+        } catch (NumberFormatException e) {
+          return Style.message(sender, ChatColor.RED.toString() + "That is not a amount.");
+        }
+
+        final Location location = ((Player) sender).getLocation();
+        if (Collector.chunkHasCollector(location)) {
+          if (Collector.isCollector(location.subtract(new Vector(0, 0.5, 0)).getBlock().getLocation())) {
+            final Collector collector = Collector.get(location.subtract(new Vector(0, 0.5, 0)).getBlock().getLocation());
+
+            collector.getAmounts().entrySet().stream().filter(entry -> collectionType == entry.getKey()).forEach(entry -> entry.setValue(amount));
+            return Style.message(sender, ChatColor.RED.toString() + "Collection type " + ChatColor.YELLOW.toString() + collectionType.toString() + ChatColor.RED.toString() + " has been set to " + ChatColor.YELLOW.toString() + amount + ChatColor.RED.toString() + ".");
           } else {
-            player.sendMessage(ChatColor.RED.toString() + "There is no collector in this chunk.");
+            return Style.message(sender, ChatColor.RED.toString() + "This is not this chunks collector.");
           }
-          return false;
+        } else {
+          return Style.message(sender, ChatColor.RED.toString() + "There is no collector in this chunk.");
         }
-
-        break;
+      }
     }
 
     return false;
