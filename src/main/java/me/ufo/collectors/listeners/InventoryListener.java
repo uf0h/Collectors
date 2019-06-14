d3795091eebcbae2b892dbe903ae74f900df5d22
uf0h <3p6824cbrf@gmail.com>
2019-06-14 19:25:31 +0100

    remove viewer on quit event

--- src/main/java/me/ufo/collectors/listeners/InventoryListener.java
+++ src/main/java/me/ufo/collectors/listeners/InventoryListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class InventoryListener implements Listener {
 
@@ -24,6 +25,11 @@ public class InventoryListener implements Listener {
     if (event.getInventory().getHolder() instanceof GUI) {
       Collector.removeViewer(event.getPlayer().getUniqueId());
     }
+  }
+
+  @EventHandler
+  public void onPlayerQuitEvent(PlayerQuitEvent event) {
+    Collector.removeViewer(event.getPlayer().getUniqueId());
   }
 
 }
